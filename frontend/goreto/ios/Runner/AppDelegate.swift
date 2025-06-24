140cb27f504deb2368b55fce1c19c32b8b0a985e
raj1008-git <macof.rj@gmail.com>
2025-06-24 05:46:04 +0545

    Google maps integrated

--- frontend/goreto/ios/Runner/AppDelegate.swift
+++ frontend/goreto/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps // ✅ Add this line
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,9 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    android:value="AIzaSyAulCLaxFFN3fwBrOaIHB8s30UbnsFRckw"/>
+    GMSServices.provideAPIKey("") // ✅ Add this line
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
